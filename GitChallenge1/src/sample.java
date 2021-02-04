d11ced49723eace61a4b5bb5f57a964c81d8ee13
Zeddboy <kunagg25@gmail.com>
2021-02-04 15:10:45 +0530

    added a feature

--- GitChallenge1/src/sample.java
+++ GitChallenge1/src/sample.java
@@ -5,5 +5,6 @@ public class sample {
 	public static void main(String[] args) {
 		System.out.println("Just a sample file");
 		System.out.println("Added features");
+		System.out.println("Added feature 2");
 	}
 }
