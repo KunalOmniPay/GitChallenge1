5ec339123ee78ccb91b9c1ce8ff0d916a56e2a38
Zeddboy <kunagg25@gmail.com>
2021-02-04 17:02:26 +0530

    commit

--- GitChallenge1/src/sample.java
+++ GitChallenge1/src/sample.java
@@ -6,7 +6,7 @@ public class sample {
 	public static void main(String[] args) {
 		System.out.println("Just a sample file");
 		System.out.println("Added features");
-		System.out.println("Added feature 2");
+		
 		//hello all
 	}
 }
